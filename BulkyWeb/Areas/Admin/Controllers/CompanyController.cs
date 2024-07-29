e4298c41b81cdb4f96f670d1e69aa562d141def9
MayurGaikwad45 <mayursg39@gmail.com>
2024-07-29 19:06:42 +0530

    Reapply "Azure Production Deploayment"
    This reverts commit 328fb7af2dc6d03ad67fc1b3552b9e4eaf431cb3.

---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,108 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment )
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            
+            return View(objCompanyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+           
+            if(id==null || id==0)
+            {
+                //create
+                return View(new Company());
+            }
+            else
+            {
+                //update
+                Company companyobj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyobj);
+            }
+           
+        }
+        [HttpPost]
+        public IActionResult Upsert(Company Companyobj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+              
+                if(Companyobj.Id ==0 )
+                {
+                    _unitOfWork.Company.Add(Companyobj);
+
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(Companyobj);
+
+                }
+                _unitOfWork.Save();
+                TempData["success"] = "Company Created Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+               
+                return View(Companyobj);
+            }
+            
+        }
+
+        
+
+       
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new {data = objCompanyList});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int?id)
+        {
+            var CompanyToBeDeleted = _unitOfWork.Company.Get(u=>u.Id == id);
+            if (CompanyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+           
+            _unitOfWork.Company.Remove(CompanyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
@@ -17,14 +21,51 @@ namespace BulkyWeb.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
+
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(inclueProperties: "Category");
             return View(productList);
         }
         public IActionResult Details(int productId)  
         {
-            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, inclueProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, inclueProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+            
+            return View(cart);
         }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId=userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u=>u.ApplicationUserId==userId && 
+            u.ProductId==shoppingCart.ProductId);
+
+            if(cartFromDb != null)
+            {
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+            }
+            TempData["success"] = "Cart Updated Successfully";
+
+           
+            return RedirectToAction("Index");
+        }
+
 
         public IActionResult Privacy()
         {
