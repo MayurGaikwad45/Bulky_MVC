a8b44addbdcfc1820b7c4cfb6fcef4061471ff56
MayurGaikwad45 <mayursg39@gmail.com>
2024-07-30 16:09:35 +0530

    addedquestionmarkincompanyinapplicationuserfile

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -21,6 +21,6 @@ namespace Bulky.Models
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
     }
 }
